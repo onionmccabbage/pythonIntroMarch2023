631f5c848ca924fda2424468761b833d9d610d38
onionmccabbage <onionmccabbage@live.com>
2023-03-28 11:45:00 +0100

    sampel using 'requests'

---
+++ sample_classes/using_requests.py
@@ -0,0 +1,24 @@
+# we can use the requests library to access external data from an API
+import requests # remember - we may need to 'pip install requests'
+
+def getData(category, id):
+    '''get data using the category and id provided'''
+    # this URL will return JSON data
+    dataUrl = f'https://jsonplaceholder.typicode.com/{category}/{id}'
+    # remember, all internet requests use http(s)
+    # 'get' is by far the mpst common eay to acces URL data
+    response = requests.get(dataUrl) # this will call the API and access the returned data
+    # the response includes the code, the headers etc.... and our data
+    # we can easily convert the JSON into a dictionary
+    response_dict = response.json() # or .text or .html or .xml ....
+    return response_dict
+
+if __name__ == '__main__':
+    '''we can call our requests method to get data'''
+    result = getData('todos', 77)
+    # print( result )
+    # careful - we have single quotes inside double quotes
+    print( f"We received {result['title']} with id {result['id']} completed {result['completed']}" )
+    # we can get photo data
+    photo_result = getData('photos', 1234)
+    print(photo_result)
