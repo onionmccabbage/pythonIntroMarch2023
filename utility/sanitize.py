b494fdd45685aefe29dafa3a8d236d477a1ef942
onionmccabbage <onionmccabbage@live.com>
2023-03-29 12:29:15 +0100

    sample with break popint(sanitize.py)

--- utility/sanitize.py
+++ utility/sanitize.py
@@ -5,10 +5,10 @@ def checkType(value):
     simpleTypes   = {int, float, bool}
     complexTypes  = {set, list, tuple, dict}
     # if type(value)==int or type(value)==float or type(value)==bool:
-    if type(value) in simpleTypes or value.isnumeric():
-        return int(float(value)) 
     if type(value) in complexTypes:
         return 0
+    if type(value) in simpleTypes or value.isnumeric():
+        return int(float(value)) 
 
 if __name__ == '__main__':
     print( checkType(3) )     # pass in a value to be validated
